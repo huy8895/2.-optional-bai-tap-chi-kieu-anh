04a051724a01dce0be71e365af3438fc9286b396
huy8895 <huy8895@gmail.com>
2020-07-23 10:53:57 +0700

    tim kiem gia sach theo ten sach bang tim kiem tuan tu

--- src/BookManger.java
+++ src/BookManger.java
@@ -1,7 +1,6 @@
 import Book.Book;
 import Book.ProgrammingBook;
 import Book.FictionBook;
-
 import java.util.Scanner;
 
 public class BookManger {
@@ -10,11 +9,16 @@ public class BookManger {
         Book[] fictionBooks = new FictionBook[5];
 //        BookManger.setBookData(programmingBooks);
         BookManger.setProgrammingBookData(programmingBooks);
+        BookManger.setFictionBookData(fictionBooks);
+
         for (Book book: programmingBooks)
             System.out.println(book);
         System.out.println("ProgrammingBook.getCountJavaBook() = " + ProgrammingBook.getCountJavaBook());
         System.out.println("ProgrammingBook.getTotalPrice() = " + ProgrammingBook.getTotalPrice());
 
+        System.out.println(BookManger.findPriceByBookName(fictionBooks,"fictionbook5"));
+
+        
 
 
     }
@@ -69,17 +73,26 @@ public class BookManger {
 
     public static void setProgrammingBookData(Book[] books){
         books[0] = new ProgrammingBook("pgb1","java book1",10.5,"tony Dung","Java","none");
-        books[1] = new ProgrammingBook("pgb1","java book1",10.5,"tony Dung","Java","none");
-        books[2] = new ProgrammingBook("pgb1","java book1",10.5,"tony Dung","Java","none");
-        books[3] = new ProgrammingBook("pgb1","java book1",10.5,"tony Dung","Java","none");
-        books[4] = new ProgrammingBook("pgb1","java book1",10.5,"tony Dung","Java","none");
+        books[1] = new ProgrammingBook("pgb2","java book2",20.5,"tony Dung","Java","none");
+        books[2] = new ProgrammingBook("pgb3","java book3",10.5,"tony Dung","Java","none");
+        books[3] = new ProgrammingBook("pgb4","java book4",10.5,"tony Dung","Java","none");
+        books[4] = new ProgrammingBook("pgb5","java book5",10.5,"tony Dung","Java","none");
     }
 
     public static void setFictionBookData(Book[] books){
-        books[0] = new FictionBook("fcb1","fictionbook1",10,"adam quan","honor");
-        books[1] = new FictionBook("fcb1","fictionbook1",10,"adam quan","honor");
-        books[2] = new FictionBook("fcb1","fictionbook1",10,"adam quan","honor");
-        books[3] = new FictionBook("fcb1","fictionbook1",10,"adam quan","honor");
-        books[4] = new FictionBook("fcb1","fictionbook1",10,"adam quan","honor");
+        books[0] = new FictionBook("fcb1","fictionbook1",10.6,"vo ba quan","khoa hoc vien tuong");
+        books[1] = new FictionBook("fcb2","my life story",20.7,"quoc tung","tam ly");
+        books[2] = new FictionBook("fcb3","fictionbook3",30.9,"chi thuy","gia dinh");
+        books[3] = new FictionBook("fcb4","my style",40.1,"van dung","fashion");
+        books[4] = new FictionBook("fcb5","fictionbook5",50.5,"adam quan","honor");
+    }
+    
+    public static double findPriceByBookName(Book[] books,String bookName){
+        for (Book book: books){
+            if (bookName.equals(book.getName())){
+                return book.getPrice();
+            }
+        }
+        return 0;
     }
 }
